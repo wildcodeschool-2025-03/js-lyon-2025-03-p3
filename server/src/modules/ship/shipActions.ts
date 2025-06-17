9331a9d895ab7e47bc347dbe7208aa27fdb76b2e
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-06-17 15:23:16 +0200

    created ship folder with shipAction and shipRepository files in it, and a shcema.sql with 3 ships

--- server/src/modules/ship/shipActions.ts
+++ server/src/modules/ship/shipActions.ts
@@ -1,16 +1,16 @@
 import type { RequestHandler } from "express";
 
 // Import access to data
-import itemRepository from "./itemRepository";
+import shipRepository from "./shipRepository";
 
 // The B of BREAD - Browse (Read All) operation
 const browse: RequestHandler = async (req, res, next) => {
   try {
     // Fetch all items
-    const items = await itemRepository.readAll();
+    const ships = await shipRepository.readAll();
 
-    // Respond with the items in JSON format
-    res.json(items);
+    // Respond with the ships in JSON format
+    res.json(ships);
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
@@ -20,16 +20,16 @@ const browse: RequestHandler = async (req, res, next) => {
 // The R of BREAD - Read operation
 const read: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch a specific item based on the provided ID
-    const itemId = Number(req.params.id);
-    const item = await itemRepository.read(itemId);
+    // Fetch a specific ship based on the provided ID
+    const shipId = Number(req.params.id);
+    const ship = await shipRepository.read(shipId);
 
-    // If the item is not found, respond with HTTP 404 (Not Found)
-    // Otherwise, respond with the item in JSON format
-    if (item == null) {
+    // If the ship is not found, respond with HTTP 404 (Not Found)
+    // Otherwise, respond with the ship in JSON format
+    if (ship == null) {
       res.sendStatus(404);
     } else {
-      res.json(item);
+      res.json(ship);
     }
   } catch (err) {
     // Pass any errors to the error-handling middleware
@@ -40,14 +40,14 @@ const read: RequestHandler = async (req, res, next) => {
 // The A of BREAD - Add (Create) operation
 const add: RequestHandler = async (req, res, next) => {
   try {
-    // Extract the item data from the request body
-    const newItem = {
-      title: req.body.title,
-      user_id: req.body.user_id,
+    // Extract the ship data from the request body
+    const newship = {
+      name: req.body.name,
+      catchphrase: req.body.catchphrase,
     };
 
-    // Create the item
-    const insertId = await itemRepository.create(newItem);
+    // Create the ship
+    const insertId = await shipRepository.create(newship);
 
     // Respond with HTTP 201 (Created) and the ID of the newly inserted item
     res.status(201).json({ insertId });
