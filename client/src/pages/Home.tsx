c9956434dd690ef6061d4c78ad9220b6839dc8c9
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-06-18 09:26:58 +0200

    before pull

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
 
-const ships = [
+/* const ships = [
   {
     name: "Exodius",
     image: "../src/assets/images/ships/ship1.png",
@@ -16,48 +17,28 @@ const ships = [
     image: "../src/assets/images/ships/ship3.png",
     text: "Toujours plus loin",
   },
-];
-
+]; */
+interface ShipsProps {
+  id: number;
+  name: string;
+  image: string;
+  catchphrase: string;
+}
 function Home() {
+  const [ships, setShips] = useState<ShipsProps[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:3310/api/ships")
+      .then((response) => response.json())
+      .then((data) => setShips(data));
+  }, []);
+  console.info(ships);
   return (
     <section>
       <figure className="ship-highlight">
-        <img src={ships[0].image} alt="exodius" className="ship-img" />
-        <figcaption className="ship-txt">
-          <h2>{ships[0].name}</h2>
-          <p> {ships[0].text}</p>
-        </figcaption>
-        <section className="button-group">
-          <button type="button" className="button-rent">
-            Reservez
-          </button>
-          <button type="button" className="button-info">
-            Plus d'infos
-          </button>
-        </section>
-      </figure>
-
-      <figure className="ship-highlight">
-        <img src={ships[1].image} alt="Pandora" className="ship-img" />
+        <img src={ships[1].image} alt="exodius" className="ship-img" />
         <figcaption className="ship-txt">
           <h2>{ships[1].name}</h2>
-          <p> {ships[1].text}</p>
-        </figcaption>
-        <section className="button-group">
-          <button type="button" className="button-rent">
-            Reservez
-          </button>
-          <button type="button" className="button-info">
-            Plus d'infos
-          </button>
-        </section>
-      </figure>
-
-      <figure className="ship-highlight">
-        <img src={ships[2].image} alt="Gérard" className="ship-img" />
-        <figcaption className="ship-txt">
-          <h2> {ships[2].name}</h2>
-          <p>{ships[2].text}</p>
+          <p> {ships[1].catchphrase}</p>
         </figcaption>
         <section className="button-group">
           <button type="button" className="button-rent">
