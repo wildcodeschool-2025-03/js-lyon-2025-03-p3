6bc533fabb877a6a24b8d3065911651f6e8a3e44
vianelbn <vb.nyamsi@yahoo.fr>
2025-06-19 14:48:42 +0200

    fix filter

---
+++ client/src/components/Filter.tsx
@@ -0,0 +1,36 @@
+import type React from "react";
+
+type FilterProps = {
+  id: string;
+  name: string;
+};
+
+const Filter: React.FC<FilterProps> = (props) => (
+  <form className="form">
+    <div className="ships">
+      {/* <label className="vaisseau-label" htmlFor={props.id}>
+        nom de vaisseau{props.name}
+      </label> */}
+      <input id={props.id} className="vaisseau-text" type="text" />
+    </div>
+    <div className="catchphrase">
+      {/* <label className="catchphrase-label" htmlFor={props.id}>
+        catchphrase{props.name}
+      </label> */}
+      <input id={props.id} className="catchphrase-text" type="text" />
+    </div>
+    <div className="btn-group">
+      <button type="button" className="btn-choix de vaisseau">
+        choix de vaisseau
+      </button>
+      <button type="button" className="btn-catchphrase">
+        catchphrase
+      </button>
+      <button type="submit" className="btn-services">
+        services
+      </button>
+    </div>
+  </form>
+);
+
+export default Filter;
