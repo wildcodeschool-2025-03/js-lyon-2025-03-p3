ed21cc7168e51c851081dfaad77263083140da5e
Hans Siemon <hanssiemon@Hanss-MacBook-Air.local>
2025-06-24 17:07:53 +0200

    partie3 de ShipDetails termine

--- client/src/pages/ShipDetails.tsx
+++ client/src/pages/ShipDetails.tsx
@@ -4,24 +4,51 @@ import "./ShipDetails.css";
 import "../UI/UX/StyleUIUX.css";
 import BtnBooked from "../UI/UX/btnBooked";
 import BtnMoreInformations from "../UI/UX/btnMoreInformations";
+import hotesse from "../assets/images/Shipdetailsimages/hotesse.png";
 
 function ShipDetails() {
   return (
     <>
-      <div className="main-content">
-        <img className="ship-img" src={Ship1} alt="ship1" />
-        <div className="text-content">
-          <h3 className="ship-brand">PICTOR</h3>
-          <h2 className="ship-title">ZETA LEONIS</h2>
-        </div>
-        <div className="buttons-wrapper">
-          <BtnBooked />
-          <BtnMoreInformations />
-        </div>
-      </div>
-      <div>
-        <img className="ship-img" src={Details} alt="details" />
-      </div>
+      <section>
+        <section className="main-content">
+          <img className="ship-img" src={Ship1} alt="ship1" />
+          <section className="text-content">
+            <h3 className="ship-brand">PICTOR</h3>
+            <h2 className="ship-title">ZETA LEONIS</h2>
+          </section>
+          <section className="buttons-wrapper">
+            <BtnBooked />
+            <BtnMoreInformations />
+          </section>
+        </section>
+        <section>
+          <img className="ship-img" src={Details} alt="details" />
+        </section>
+        <section>
+          <h2 className="gap-start">Nos Stewardess...</h2>
+          <h3 className="gap-end">....à votre service</h3>
+        </section>
+        <section className="Shippart3">
+          <section className="texte">
+            <p className="petittexte">
+              Lorem ipsum dolor sit amet. Aut perferendis esse non laboriosam
+              mollitia et tempora dicta ab doloribus quibusdam sed voluptas
+              veniam ea quae corrupti! Sed nulla recusandae aut sequi iste sed
+              esse recusandae est assumenda voluptatem! Et voluptas quia qui
+              incidunt quod aut nulla magni qui libero maiores quo eius nostrum.
+              Non facilis facere et enim harum eum nesciunt nihil a rerum animi
+              qui dolores nihil eos sunt odio.
+            </p>
+          </section>
+          <section>
+            <img
+              src={hotesse}
+              alt="hôtesse de l'espace"
+              className="stewardess-img"
+            />
+          </section>
+        </section>
+      </section>
     </>
   );
 }
