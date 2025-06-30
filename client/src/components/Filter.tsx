5fe22c4a1a916dca2b1635dd875082c4fbcd7836
vianelbn <vb.nyamsi@yahoo.fr>
2025-06-30 16:32:21 +0200

    stylize filter

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,36 +1,71 @@
-import type React from "react";
+import "./Filter.css";
 
-type FilterProps = {
-  id: string;
-  name: string;
-};
+function Filter() {
+  return (
+    <form className="filter-form">
+      <section id="group-ships">
+        <section id="ships">
+          <button type="button" className="btn-ship">
+            Corvette
+          </button>
+          <button type="button" className="btn-ship">
+            Frégate
+          </button>
+          <button type="button" className="btn-ship">
+            Man'o'war
+          </button>
+        </section>
 
-const Filter: React.FC<FilterProps> = (props) => (
-  <form className="form">
-    <div className="ships">
-      {/* <label className="vaisseau-label" htmlFor={props.id}>
-        nom de vaisseau{props.name}
-      </label> */}
-      <input id={props.id} className="vaisseau-text" type="text" />
-    </div>
-    <div className="catchphrase">
-      {/* <label className="catchphrase-label" htmlFor={props.id}>
-        catchphrase{props.name}
-      </label> */}
-      <input id={props.id} className="catchphrase-text" type="text" />
-    </div>
-    <div className="btn-group">
-      <button type="button" className="btn-choix de vaisseau">
-        choix de vaisseau
-      </button>
-      <button type="button" className="btn-catchphrase">
-        catchphrase
-      </button>
-      <button type="submit" className="btn-services">
-        services
-      </button>
-    </div>
-  </form>
-);
+        <section id="withdrawal-return">
+          <section id="withdrawal-return-text-select">
+            <p id="withdrawalAndReturnText">Retrait et retour</p>
+            <select name="withdrawalAndReturn" id="select-withdrawal-return">
+              <option value="withdrawal">Laguna Vista - Texas - US</option>
+              <option value="return">Flamingo - Floride -US</option>
+              <option value="return">Mana - Guyane - FR</option>
+              <option value="return">Ras el Khaïmah - AE</option>
+              <option value="return">Zhangzhou - Fujian - CN</option>
+            </select>
+          </section>
+
+          <button type="button" id="btn-withdrawal-return">
+            + lieu de retour différent
+          </button>
+        </section>
+      </section>
+
+      <section id="group-services">
+        <h2>Services</h2>
+        <section id="btn-group-services">
+          <button type="button" className="btn-service">
+            Premium
+          </button>
+          <button type="button" className="btn-service">
+            Gold
+          </button>
+          <button type="button" className="btn-service">
+            Platine
+          </button>
+        </section>
+      </section>
+      <section id="group-date">
+        <section id="btn-group-date">
+          <label htmlFor="start" className="label-date">
+            Date de départ
+            <input type="date" id="start" name="trip-start" />
+          </label>
+
+          <label htmlFor="end" className="label-date">
+            Date de retour
+            <input type="date" id="end" name="trip-end" />
+          </label>
+        </section>
+        <button type="button" id="btn-seeships">
+          Voir les vaisseaux
+        </button>
+      </section>
+    </form>
+  );
+}
 
 export default Filter;
