24bc7337c2717cadfa871f91364f6cd728cc4d68
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-07-01 14:30:38 +0200

    created user table in schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,8 +5,18 @@ create table ship (
   catchphrase varchar(200) not null
 );
 
+create table user (
+  id int unsigned primary key auto_increment not null,
+  email varchar(100) not null UNIQUE,
+  password varchar(100) not null
+);
+
 insert into ship (id, name, image, catchphrase)
 values
   (1, "Zeta Leonis", "/upload/ship1.png", "Reussissez vos spaceTrips"),
   ( 2, "Alpha Andromedae", "/upload/ship2.png", "Laissez vous emporter"),
   (3, "Theta Eridani", "/upload/ship3.png", "Plus vite que la lumière");
+
+  insert into user (id, email, password)
+  values
+  (1, "test@test.com", "test123nohash");
