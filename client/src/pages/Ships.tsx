9c7803f96fca14f46ec69a5b1e1e269e68a12860
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-07-01 11:10:07 +0200

    created the ShipCard component, and added an iconCard folder containing the icons needed

--- client/src/pages/Ships.tsx
+++ client/src/pages/Ships.tsx
@@ -1,9 +1,11 @@
 import Filter from "../components/Filter";
+import ShipCard from "../components/ShipCard";
 
 function Ships() {
   return (
     <>
       <Filter id="filtre-ships" name="Ships Filter" />
+      <ShipCard />
     </>
   );
 }
