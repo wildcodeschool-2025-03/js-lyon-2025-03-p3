79ba9e3610bb450bf9c6f8f6c2480644c9d16db7
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-07-06 15:14:59 +0200

    structured the AddShip page and files related to it

--- client/src/pages/AddShip.tsx
+++ client/src/pages/AddShip.tsx
@@ -1,7 +1,15 @@
 import ShipForm from "../components/ShipForm";
+import "./AddShip.css";
 
 function AddShip() {
-  return <ShipForm />;
+  return (
+    <>
+      <section className="addShip-wrapper">
+        <h2 className="addShip-title">Ajoutez votre vaisseau spatial</h2>
+        <ShipForm />
+      </section>
+    </>
+  );
 }
 
 export default AddShip;
