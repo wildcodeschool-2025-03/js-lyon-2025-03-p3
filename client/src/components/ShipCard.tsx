1e615dba9fe71488b3bef6981427fbdaa0fc842c
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-07-06 12:46:17 +0200

    fetched our api, then displayed the ShipCards and modified the css file to match our names and images, then modified te filter margins for a better display

--- client/src/components/ShipCard.tsx
+++ client/src/components/ShipCard.tsx
@@ -7,39 +7,45 @@ import capacity from "../assets/images/iconCard/capacity.svg";
 import bed from "../assets/images/iconCard/hotel.svg";
 import person from "../assets/images/iconCard/person.svg";
 import speed from "../assets/images/iconCard/speed.svg";
-import ship1 from "../assets/images/ships/ship1.png";
 
-function ShipCard() {
+interface ShipProps {
+  name: string;
+  image: string;
+}
+function ShipCard({ name, image }: ShipProps) {
+  const baseURL = import.meta.env.VITE_API_URL;
   return (
     <figure className="ship-card">
-      <div className="logo-name-wrapper">
-        <img
-          className="logo-manufacturer"
-          src={tucanaYellow}
-          alt="logo manufacturer"
-        />
-        <h2 className="ship-name">Ship Test 1</h2>
-      </div>
-      <div className="wrapper-infos">
-        <section className="ship-infos">
-          <div className="icon-info">
-            <img src={capacity} alt="capacity" /> <p>2</p>
-          </div>
-          <div className="icon-info">
-            <img src={bed} alt="bed" /> <p>1</p>
-          </div>
-          <div className="icon-info">
-            <img src={person} alt="person" /> <p>1</p>
-          </div>
-          <div className="icon-info">
-            <img src={battery} alt="battery" /> <p>2 UA</p>
-          </div>
-        </section>
-        <div className="icon-info large">
-          <img src={speed} alt="speed" /> <p>Supraluminique type 4</p>
+      <section className="infos-top">
+        <div className="logo-name-wrapper">
+          <img
+            className="logo-manufacturer"
+            src={tucanaYellow}
+            alt="logo manufacturer"
+          />
+          <h2 className="ship-name">{name}</h2>
         </div>
-      </div>
-      <img src={ship1} alt={ship1} />
+        <div className="wrapper-logos">
+          <section className="ship-infos">
+            <div className="icon-info">
+              <img src={capacity} alt="capacity" /> <p>2</p>
+            </div>
+            <div className="icon-info">
+              <img src={bed} alt="bed" /> <p>1</p>
+            </div>
+            <div className="icon-info">
+              <img src={person} alt="person" /> <p>1</p>
+            </div>
+            <div className="icon-info">
+              <img src={battery} alt="battery" /> <p>2 UA</p>
+            </div>
+          </section>
+          <div className="icon-info large">
+            <img src={speed} alt="speed" /> <p>Supraluminique type 4</p>
+          </div>
+        </div>
+      </section>
+      <img src={`${baseURL}${image}`} alt={name} />
       <div className="btn-wrapper">
         <BtnBooked />
         <BtnMoreInformations />
