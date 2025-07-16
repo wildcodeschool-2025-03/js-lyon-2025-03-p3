bc317f2619ab4dea59a4b9db453b58467398f77b
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-07-16 14:09:51 +0200

    fused Admin and notAuth component

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import NotAuth from "../components/NotAuth";
+
 function Admin() {
-  return <h1>Je suis un admin</h1>;
+  const [isAuth, setIsAuth] = useState(Boolean);
+  const baseURL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch(`${baseURL}/api/me`, {
+          credentials: "include", // send the cookie to the server to verify the credentials
+        });
+
+        if (res.ok) {
+          setIsAuth(true);
+        } else {
+          setIsAuth(false);
+        }
+      } catch (err) {
+        setIsAuth(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+  return isAuth ? <h2>Page Admin</h2> : <NotAuth />;
 }
 
 export default Admin;
